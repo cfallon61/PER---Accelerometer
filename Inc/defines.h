36816273dc5c4340b7df42ead3447e9bf6448bff
cfallon61 <fallon2@purdue.edu>
2018-02-02 15:25:33 -0500

    Finally found the right project files

---
+++ Inc/defines.h
@@ -0,0 +1,55 @@
+/*
+ /*
+ * defines.h
+ *
+ *  Created on: Dec 30, 2017
+ *      Author: Vert
+ */
+
+#ifndef DEFINES_H_
+#define DEFINES_H_
+
+
+#define X_L_ADDR					0x28
+#define X_H_ADDR					0x29
+#define Y_L_ADDR					0x2A
+#define Y_H_ADDR					0x2B
+#define Z_L_ADDR					0x2C
+#define Z_H_ADDR					0x2D
+
+#define CTRL_REG_4					0x20
+#define ENABLE_XYZ					0x67
+#define CTRL_REG_5					0x24
+
+
+
+#define MEMS_PIN					GPIO_PIN_3
+#define MEMS_PORT					GPIOE
+
+
+//LEDs
+#define RED_LED 					LD5_Pin
+#define ORANGE_LED					LD3_Pin
+#define BLUE_LED 					LD6_Pin
+#define GREEN_LED 					LD4_Pin
+#define LED_PORT 					GPIOD
+
+
+typedef enum{
+	Filter_800Hz,
+	Filter_400Hz,
+	Filter_200Hz,
+	Filter_50Hz,
+
+}Filter_t;
+
+typedef enum {
+	Sensitivity_2G,
+	Sensitivity_4G,
+	Sensitivity_6G,
+	Sensitivity_8G,
+	Sensitivity_16G,
+
+}Sensitivity_t;
+
+#endif /* DEFINES_H_ */
